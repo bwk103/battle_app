abdda7dae692816136a71ff16568243a6e9e8f79
Ben Kielty <bwk103@gmail.com>
2017-12-31 16:59:14 +0000

    User Story 1 Complete
    On visiting '/' players can add their names to the game.  On submitting
    the form, the application displays the players' names.  Added feature
    test.

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class BattleApp < Sinatra::Base
 
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @player1 = params['player_1_name']
+    @player2 = params['player_2_name']
+    erb(:play)
   end
 
 end
--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "Testing infrastructure", :type => :feature do
-  it "checks the homepage is working" do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
