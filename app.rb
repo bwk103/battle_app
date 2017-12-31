cefca3eb140b2a56f8421d020f7904149a39c830
Ben Kielty <bwk103@gmail.com>
2017-12-31 17:16:49 +0000

    Refactor app.rb
    Refactored routes so that post /names now redirects to get /play.
    Enabled session and store player names in session.

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra/base'
 
 class BattleApp < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/names' do
-    @player1 = params['player_1_name']
-    @player2 = params['player_2_name']
+  get '/play' do
+    @player1 = session['player1']
+    @player2 = session['player2']
     erb(:play)
+  end
+
+  post '/names' do
+    session['player1'] = params['player_1_name']
+    session['player2'] = params['player_2_name']
+    redirect('/play')
   end
 
 end
